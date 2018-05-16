144ca52a482ee0aac20c429cc216347a57c8828b
unknown <esraaezzat545@gmail.com>
2018-05-16 22:19:14 +0200

    k,hn

--- src/app/Services/contact.service.ts
+++ src/app/Services/contact.service.ts
@@ -32,7 +32,7 @@ export class ContactService {
     this.alpha = ['A','B','C','D','C','E','F','G','H','I','J','K','L','M','N','O','P',
                    'Q','R','S','T','U','V','W','X','Y','Z'];
     
-                       
+                      
                   
 
   }
